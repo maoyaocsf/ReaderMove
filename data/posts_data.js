a020f7717acb3c9777b4a29ca921acecb927208e
Admin <Admin>
2017-09-21 20:46:11 +0800

    first commit

---
+++ data/posts_data.js
@@ -0,0 +1,42 @@
+var local_database=[
+  {
+    data:"Sep 18 2016",
+    title:"正是虾肥蟹壮时",
+    imgSrc:'/images/post/crab.png',
+    avatar:'/images/avatar/1.png',
+    author:'林白衣',
+    content:'开海近一个月了,捕捞量增加,中秋节又刚刚过去,虾蟹此时最为肥美,国庆长假到海边尝尝最地道、最新鲜的海鲜,恐怕不只是吃货们的向往吧。可是您知道到了咱们的沿海城市青岛、烟台、威海和日照后,去哪儿能吃。',
+    reading:'112',
+    collection:'96',
+    dateTime: '24小时前',
+    headSrc: '/images/post/crab.png',
+    postId:0,
+    music:{
+      url:'http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38',
+      title:'鬼迷心窍',
+      coverImg:'http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000'
+    }
+  },
+  {
+    data: "Nav 25 2016",
+    title: "比利·林恩的中场故事",
+    author: '林的',
+    imgSrc: '/images/post/bl.png',
+    avatar: '/images/avatar/2.png',
+    content: '《比利·林恩的中场战事》是由美国索尼电影娱乐公司发行，由李安执导，乔·阿尔文、克里斯汀·斯图尔特、克里斯·塔克、加内特·赫德兰联合主演剧情片。《比利·林恩的中场战事》根据本·芳汀同名小说改编，讲述了在伊拉克战争中的美国士兵比利·林恩与战友战胜归来并被誉为美国英雄，在一场橄榄球公开赛的中场表演过程中，揭露这群士兵在战场上真实经历的故事。该片于2016年11月11日以120帧、60帧、24帧/3D规格在中国上映。',
+    reading: '112',
+    collection: '96', 
+    headSrc:'/images/post/bl.png',
+    dateTime:'24小时前',
+    postId:1,
+    music: {
+      url: 'http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38',
+      title: '沉默是金',
+      coverImg: 'http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000'
+    }
+
+  }
+];
+module.exports={
+  postList: local_database
+}
